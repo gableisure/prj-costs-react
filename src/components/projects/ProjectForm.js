9d8b8c7d2ba0f59cd64123d3ab353b77ec079c76
Gabriel Ribeiro de Araújo <gableisure@sempreceub.com>
2021-09-29 00:12:20 -0300

    [feat] add form for create project

---
+++ src/components/projects/ProjectForm.js
@@ -0,0 +1,30 @@
+import styles from './ProjectForm.module.css'
+import Input from '../form/Input'
+import Select from '../form/Select';
+import SubmitButton from '../form/SubmitButton';
+
+function ProjectForm() {
+    return (
+        <form className={styles.form}>
+            <Input 
+                type="text"
+                text="Nome do projeto"
+                name="name"
+                placeholder="Insira o nome do projeto"
+            />
+            <Input 
+                type="number"
+                text="Orçamento do projeto"
+                name="budget"
+                placeholder="Insira o orçamento total"
+            />
+            <Select 
+                name="category_id" 
+                text="Selecione a categoria"
+            />
+           <SubmitButton text="Criar projeto"/>
+        </form>
+    )
+}
+
+export default ProjectForm;
